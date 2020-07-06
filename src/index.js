aea9aa62237c0f808fc2fd1f4578e868f0dd1f05
Laur <laur.gabriel45@gmail.com>
2020-07-06 20:58:40 +0300

    setting up firebase and getting data from it with componentDidMount

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+const firebase = require('firebase')
+require('firebase/firestore')
+
+firebase.initializeApp({
+  apiKey: "AIzaSyDZshdVfmeJC4FcyIMgzIGVQD6SFN5Bw5c",
+  authDomain: "evernote-clone-1823c.firebaseapp.com",
+  databaseURL: "https://evernote-clone-1823c.firebaseio.com",
+  projectId: "evernote-clone-1823c",
+  storageBucket: "evernote-clone-1823c.appspot.com",
+  messagingSenderId: "651236721928",
+  appId: "1:651236721928:web:e12bb094ad3a0bf55958bd",
+  measurementId: "G-D3FDPQGXV4"
+});
+firebase.analytics();
 
 ReactDOM.render(
   <React.StrictMode>
